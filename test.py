d00ef59626785275480ccd4da6bb134ed0aa9a1a
gamaliellobato <gamaliel.lobato.eon@gmail.com>
2021-11-18 15:50:54 -0600

    Se agrega proyecto security test python

---
+++ test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from single_test import SingleTest
+
+class Test(unittest.TestCase):
+
+    def unit_test(self):
+    	x_api_key = "your_x_api_key"
+    	password = "your_keystore_password"
+    	pathKeypair = "/your_path/keypair.p12"
+    	pathCertificateCdc = "/your_path/cdc_cert.pem"
+    	basePath = "the_url"
+    	jsonRequest = '{"hola":"hola" }'
+
+    	single_test = SingleTest.prueba(self, x_api_key, password, pathKeypair, pathCertificateCdc, basePath, jsonRequest)
+    	self.assertTrue(single_test)
+
+unittest.main()

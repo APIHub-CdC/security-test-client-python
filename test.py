d57df0718c48a9f4fe07930c9a4d8f8f45106071
gamaliellobato <gamaliel.lobato.eon@gmail.com>
2024-02-21 12:47:11 -0600

    WIP:fix unit test

--- test.py
+++ test.py
@@ -2,17 +2,19 @@ import unittest
 
 from single_test import SingleTest
 
+
 class Test(unittest.TestCase):
 
-    def unit_test(self):
-    	x_api_key = "your_x_api_key"
-    	password = "your_keystore_password"
-    	pathKeypair = "/your_path/keypair.p12"
-    	pathCertificateCdc = "/your_path/cdc_cert.pem"
-    	basePath = "the_url"
-    	jsonRequest = '{"hola":"hola" }'
-
-    	single_test = SingleTest.prueba(self, x_api_key, password, pathKeypair, pathCertificateCdc, basePath, jsonRequest)
-    	self.assertTrue(single_test)
+	def test_pruebas(self):
+		x_api_key = "your_x_api_key"
+		password = "your_keystore_password"
+		pathKeypair = "/your_path/keypair.p12"
+		pathCertificateCdc = "/your_path/cdc_cert.pem"
+		basePath = "the_url"
+		jsonRequest = '{"hola":"hola" }'
+  
+		single_test = SingleTest.prueba(self, x_api_key, password, pathKeypair, pathCertificateCdc, basePath, jsonRequest)
+		self.assertTrue(single_test)
+  
 
 unittest.main()
